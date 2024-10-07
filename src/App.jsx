d5eb987688a5bc4dc8c4caa9a58c781a2ab85745
Aysen Aydin <aysen904@gmail.com>
2024-10-07 13:09:42 +0300

    tailwind instal and component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import IntroSection from "./components/IntroSection";
+import SkillsSection from "./components/SkillsSection";
+import ProfileSection from "./components/ProfileSection";
+import ProjectsSection from "./components/ProjectsSection";
+import FooterSection from "./components/FooterSection";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App bg-light-white dark:bg-dark-gray">
+        <IntroSection />
+        <SkillsSection />
+        <ProfileSection />
+        <ProjectsSection />
+        <FooterSection />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
